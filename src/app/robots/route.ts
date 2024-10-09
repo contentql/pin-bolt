292afbcb5c09a08f02777c74362c605daa0fea3f
pavanbhaskar <pavanbhaskardev@gmail.com>
2024-10-09 14:32:23 +0530

    chore: add console in robots route

--- src/app/robots/route.ts
+++ src/app/robots/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from 'next/server'
 export const dynamic = 'force-dynamic'
 
 export async function GET() {
+  console.log('robots', { payloadURL: process.env.PAYLOAD_URL })
+
   const robotsTxt = `
 User-agent: *
 Allow: /
