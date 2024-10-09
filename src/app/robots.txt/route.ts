5c229e3f8cf95fc34556f07e580bcae8fde0418b
pavanbhaskar <pavanbhaskardev@gmail.com>
2024-10-09 14:43:30 +0530

    fix: spacing change in robots txt file

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -5,12 +5,10 @@ export const dynamic = 'force-dynamic'
 export async function GET() {
   console.log('robots', { payloadURL: process.env.PAYLOAD_URL })
 
-  const robotsTxt = `
-User-agent: *
+  const robotsTxt = `User-Agent: *
 Allow: /
 
-Sitemap: ${process.env.PAYLOAD_URL}/sitemap.xml
-`
+Sitemap: ${process.env.PAYLOAD_URL}`
 
   return new NextResponse(robotsTxt, {
     status: 200,
