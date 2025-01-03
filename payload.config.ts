877573b6cdf3702c58938ebdb56e788dcdda0d0d
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-01-03 18:17:46 +0530

    chore: added csrf

--- payload.config.ts
+++ payload.config.ts
@@ -29,6 +29,19 @@ const dirname = path.dirname(filename)
 //   return [url]
 // }
 
+const convertRailwayURL = (url: any) => {
+  const railwayDomain = '.up.railway.app'
+  const contentqlDomain = '.contentql.io'
+
+  // Check if the URL ends with .up.railway.app or contains it
+  if (url.includes(railwayDomain)) {
+    return url.replace(railwayDomain, contentqlDomain)
+  }
+
+  // Return the original URL if it doesn't match
+  return url
+}
+
 // console.log('CSRF-list', getCSRFList())
 
 export default cqlConfig({
@@ -40,8 +53,8 @@ export default cqlConfig({
       },
     },
   },
-  cors: '*',
-  csrf: undefined,
+  cors: [env.PAYLOAD_URL, convertRailwayURL(env.PAYLOAD_URL)],
+  csrf: [env.PAYLOAD_URL, convertRailwayURL(env.PAYLOAD_URL)],
 
   baseURL: env.PAYLOAD_URL,
 
