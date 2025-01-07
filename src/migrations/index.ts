e40dbb7cf5c7a4b60e56977bd291000cc774c51e
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-01-07 17:37:05 +0530

    chore: updated to @contentql/core latest version

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20241213_084215 from './20241213_084215';
 import * as migration_20250107_091534 from './20250107_091534';
 import * as migration_20250107_104212 from './20250107_104212';
 import * as migration_20250107_105053 from './20250107_105053';
+import * as migration_20250107_115055 from './20250107_115055';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20250107_105053.up,
     down: migration_20250107_105053.down,
-    name: '20250107_105053'
+    name: '20250107_105053',
+  },
+  {
+    up: migration_20250107_115055.up,
+    down: migration_20250107_115055.down,
+    name: '20250107_115055'
   },
 ];
