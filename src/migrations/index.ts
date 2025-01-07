befd2dc671bc4f264759e120e4be890f86446be9
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-01-07 16:20:18 +0530

    chore: added migration to change group-menu-link name

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20241213_084215 from './20241213_084215';
 import * as migration_20250107_091534 from './20250107_091534';
+import * as migration_20250107_104212 from './20250107_104212';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250107_091534.up,
     down: migration_20250107_091534.down,
-    name: '20250107_091534'
+    name: '20250107_091534',
+  },
+  {
+    up: migration_20250107_104212.up,
+    down: migration_20250107_104212.down,
+    name: '20250107_104212'
   },
 ];
